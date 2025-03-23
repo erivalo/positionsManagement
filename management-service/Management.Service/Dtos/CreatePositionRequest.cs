11026e5846fbb1dac6661e28a531f05495caa676
erivalo <erivalo007@gmail.com>
2025-03-23 17:27:24 -0400

    Add CreatePositionRequest Dto
    - Add db file

--- management-service/Management.Service/Dtos/CreatePositionRequest.cs
+++ management-service/Management.Service/Dtos/CreatePositionRequest.cs
@@ -1,2 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+
 namespace Management.Service.Dtos;
-public record CreatePositionRequest(int PositionNumber, string Title, decimal Budget, int RecruiterId, int DepartmentId);
+public class CreatePositionRequest
+{
+  [Required]
+  public int PositionNumber { get; set; }
+  [Required]
+  public string Title { get; set; } = string.Empty;
+  [Required]
+  [Range(1, int.MaxValue, ErrorMessage = "Only positive number allowed")]
+  public decimal Budget { get; set; }
+
+  [Required]
+  public int RecruiterId { get; set; }
+  [Required]
+  public int DepartmentId { get; set; }
+}
